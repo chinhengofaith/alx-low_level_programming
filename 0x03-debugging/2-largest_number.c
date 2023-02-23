2d8a574a7f470282b2133f38f577a15064d38d52
chinhengofaith <chinhengofaith@gmail.com>
2023-02-23 12:20:13 -0800

    printing the largest number of the three

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,2 +1,34 @@
-#!/bin/bash
-gcc -E -o c $CFILE
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+
+	int largest;
+
+	if (a > b && b > c)
+	{
+		largest = a;
+	}
+	else if (b > a && a > c)
+	{
+		largest = b;
+	}
+	else if (c > a && c > b)
+	{
+		largest = c;
+	}
+	else
+	{
+		printf("all are equal or any two values are equal")
+	}
+	return (largest);
+}
