7356556cad86d8d43954ec8fc8a957260f7bfaf9
chinhengofaith <chinhengofaith@gmail.com>
2023-02-24 12:58:13 -0800

    function to multiply 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function that prints the numbers, from 0 to 9
+ * @a: the first parameter
+ * @b: the second parameter
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 void _putchar(void);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
