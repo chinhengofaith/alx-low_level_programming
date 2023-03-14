8d6594cb93ff374f046552748b04e44082fda281
chinhengofaith <chinhengofaith@gmail.com>
2023-03-14 20:55:23 +0000

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * _strdup -  function that returns a pointer to a newly allocated space in memory,
+ * _strdup -  function that returns a pointer to a newly allocated space
  * @str: string to copy
  * Return: pointer to array or null
  */
