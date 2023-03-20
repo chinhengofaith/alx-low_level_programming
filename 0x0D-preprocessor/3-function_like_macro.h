bd76046b1c6d376d14ed6c9e30757c8703332d7a
chinhengofaith <chinhengofaith@gmail.com>
2023-03-20 22:30:36 +0000

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef ABS_FUNCTION_LIKE_MACRO
 #define ABS_FUNCTION_LIKE_MACRO
 
-#define ABS(x) ((x<0)?(-x):(x))
+#define ABS(x) ((x) >= 0 ? (x) : ((x) * -1))
 
 #endif /* ABS_FUNCTION_LIKE_MACRO*/
