3c7124ca9207ccd4dd61324a6563ec90de38c1a6
chinhengofaith <chinhengofaith@gmail.com>
2023-03-21 21:00:17 +0000

    2-print_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,9 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
-int _putchar(char c)
-#endif /* #ifndef DOG_H */
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /* #ifndef DOG_H */
