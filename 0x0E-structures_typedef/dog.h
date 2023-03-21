814f7dac29a07f943db4f00ef0c36284a9face64
chinhengofaith <chinhengofaith@gmail.com>
2023-03-21 19:36:59 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: type member 1
+ * @age: type member 2
+ * @owner: type member 3
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
