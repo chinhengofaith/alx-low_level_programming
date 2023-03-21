4391b70bb1b156411666151bceb55e254ab8bdaa
chinhengofaith <chinhengofaith@gmail.com>
2023-03-21 20:22:05 +0000

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,5 +13,6 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
-#endif /* #ifndef DOG_H */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif /* #ifndef DOG_H */
+/*void init_dog(struct dog *d, char *name, float age, char *owner);*/
