48b4fd537fbce916d9c9033620e644bdf09f652a
chinhengofaith <chinhengofaith@gmail.com>
2023-04-03 15:29:39 -0700

    added if to 0-print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,16 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t i;
-
-	for (i = 0; h; i++)
+	
+	if (h != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		for (i = 0; h; i++)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+		}
 	}
+	else
+		return (0);
 	return (i);
 }
